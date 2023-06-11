dd8a86dda774728926553773466935a196dcd5c5
ansachuk <ansachuk@icloud.com>
2023-06-11 12:21:47 +0300

    add redux-persist

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,4 @@
-import { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { setFromLS } from "redux/contactsSlice";
+import { useSelector } from "react-redux";
 
 import ContactForm from "../ContactForm/ContactForm";
 import Filter from "../Filter/Filter";
@@ -9,27 +6,8 @@ import ContactList from "../ContactList/ContactList";
 
 import css from "./App.module.css";
 
-const LOCAL_KEY = "contacts";
-
 export function App() {
 	const contacts = useSelector(({ contacts }) => contacts);
-	const disp = useDispatch();
-
-	let first = useRef(1);
-
-	useEffect(() => {
-		const localContacts = JSON.parse(localStorage.getItem(LOCAL_KEY));
-		if (localContacts && first.current === 1) {
-			disp(setFromLS(localContacts));
-
-			first.current += 1;
-			console.log("first");
-		}
-	}, [disp]);
-
-	useEffect(() => {
-		localStorage.setItem(LOCAL_KEY, JSON.stringify(contacts));
-	}, [contacts]);
 
 	return (
 		<>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import contactsReducer from "./contactsSlice";
 import filterReducer from "./filterSlice";
 
-const store = configureStore({
-	reducer: {
-		contacts: contactsReducer,
-		filter: filterReducer,
-	},
+const persistConfig = {
+	key: "contacts",
+	storage,
+	// blacklist: ["filter"],
+};
+
+const reducer = combineReducers({
+	contacts: contactsReducer,
+	filter: filterReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+	reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
 export default store;
